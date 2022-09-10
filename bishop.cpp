ceab1b906f8ddfe4d517bf9f289878b3eb516d96
akashgoyal79 <79572844+akashgoyal79@users.noreply.github.com>
2022-09-10 18:01:28 +0530

    Add files via upload

---
+++ bishop.cpp
@@ -0,0 +1,29 @@
+class BishopPiece : public GamePiece
+{
+public:
+    BishopPiece(char cColor) : GamePiece(cColor) {}
+    ~BishopPiece() {}
+private:
+    virtual char GetPiece() {
+        return 'B';
+    }
+    bool AreSquaresLegal(int iSrcRow, int iSrcCol, int iDestRow, int iDestCol, GamePiece* GameBoard[8][8]) {
+        if ((iDestCol - iSrcCol == iDestRow - iSrcRow) || (iDestCol - iSrcCol == iSrcRow - iDestRow)) {
+            // Make sure that all invervening squares are empty
+            int iRowOffset = (iDestRow - iSrcRow > 0) ? 1 : -1;
+            int iColOffset = (iDestCol - iSrcCol > 0) ? 1 : -1;
+            int iCheckRow;
+            int iCheckCol;
+            for (iCheckRow = iSrcRow + iRowOffset, iCheckCol = iSrcCol + iColOffset;
+                iCheckRow !=  iDestRow;
+                iCheckRow = iCheckRow + iRowOffset, iCheckCol = iCheckCol + iColOffset)
+            {
+                if (GameBoard[iCheckRow][iCheckCol] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+};
